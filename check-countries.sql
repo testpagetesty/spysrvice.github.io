0a7a685b77f8eecb9dc8025561b59c5cced6dc0b
testpagetesty <arsenrenok31@gmail.com>
2026-01-04 19:52:08 +0300

    Удалены все файлы из репозитория

--- check-countries.sql
+++
@@ -1,6 +0,0 @@
--- Проверка количества стран в базе данных
-SELECT COUNT(*) as total_countries FROM countries;
-
--- Показать все страны
-SELECT code, name FROM countries ORDER BY name;
-
--- src/app/api/test/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server'
-
-export async function GET() {
-  return NextResponse.json({ message: 'Test API works!' })
-}
-
-export async function POST() {
-  return NextResponse.json({ message: 'POST works too!' })
-}
